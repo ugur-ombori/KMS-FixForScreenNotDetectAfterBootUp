a30cddcfb046e44efc68ccf4da0c29a3caa42061
ugur-ombori <97463402+ugur-ombori@users.noreply.github.com>
2022-04-01 16:21:51 +0300

    update

--- repair_script.sh
+++ repair_script.sh
@@ -29,8 +29,17 @@ if [ "$SYSTEM_ARCH" = "amd64" ] ; then
             echo " + KMS file verified succesful!"
             #backup the old KMS file for any restore operation
             cp "$AMD_KMS_CONF_FILE" "$HOME_OMBORI/.old_gridos.conf-$DATE"
-            #ADD required KMS settings at the end of the KMS file
-            sed -i '$a hdmi_force_hotplug=1\nhdmi_group=1\hdmi_mode=16' "$AMD_KMS_CONF_FILE"
+
+            #Check the settings applied before
+            if grep -q "hdmi_force_hotplug=1\|hdmi_group=1\|hdmi_mode=16" "$AMD_KMS_CONF_FILE" ; then
+                #ADD required KMS settings at the end of the KMS file
+                sed -i '$a hdmi_force_hotplug=1\nhdmi_group=1\nhdmi_mode=16' "$AMD_KMS_CONF_FILE"
+            else
+                echo " - ERR! The changes are applied before!"
+                echo " - No change applied!"
+            fi
+
+            
 
         else
             echo " - ERR! KMS file could NOT verified!!"
@@ -57,6 +66,16 @@ elif [ "$SYSTEM_ARCH" = "arm64" ] ; then
             #backup the old KMS file for any restore operation
             cp "$ARM_KMS_CONF_FILE" "$HOME_OMBORI/.old_config.txt-$DATE"
             #ADD required KMS settings at the end of the KMS file
+
+            #Check the settings applied before
+            if grep -q "hdmi_force_hotplug=1\|hdmi_group=1\|hdmi_mode=16" "$ARM_KMS_CONF_FILE" ; then
+                #ADD required KMS settings at the end of the KMS file
+                sed -i '$a hdmi_force_hotplug=1\nhdmi_group=1\nhdmi_mode=16' "$ARM_KMS_CONF_FILE"
+            else
+                echo " - ERR! The changes are applied before!"
+                echo " - No change applied!"
+            fi
+
             sed -i '$a hdmi_force_hotplug=1\nhdmi_group=1\hdmi_mode=16' "$ARM_KMS_CONF_FILE"
 
         else
