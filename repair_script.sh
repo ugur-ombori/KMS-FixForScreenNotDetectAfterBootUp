1ba8f5a867e46f070b5f54592fb580567b392c30
ugur-ombori <97463402+ugur-ombori@users.noreply.github.com>
2022-04-01 15:13:02 +0300

    POSIX compatible fixes

--- repair_script.sh
+++ repair_script.sh
@@ -4,27 +4,26 @@
 AMD_KMS_CONF_FILE=/mnt/loader/entries/gridos.conf
 ARM_KMS_CONF_FILE=/mnt/config.txt
 
-DATE=`TZ='Europe/Stockholm' date +%Y-%b-%d.%H:%M:%S`
-HOME_OMBORI=`getent passwd ombori | cut -d: -f6`
+DATE=$(TZ='Europe/Stockholm' date +%Y-%b-%d.%H:%M:%S)
+HOME_OMBORI=$(getent passwd ombori | cut -d: -f6)
 
 #Boot devices for arm and amd (diffrence comes from using disk and sd card)
 AMD_BOOT=/dev/sda1
 ARM_BOOT=/dev/mmcblk0p1
 
-SYSTEM_ARCH=`dpkg --print-architecture`
+SYSTEM_ARCH=$(dpkg --print-architecture)
 echo "System Architecture: $SYSTEM_ARCH"
 
 #AMD64 arch systems
-if [ "$SYSTEM_ARCH" == "amd64" ] then
-
-    mount /dev/sda1 /mnt
-    if [ -f "$AMD_KMS_CONF_FILE" ]; then
+if [ "$SYSTEM_ARCH" = "amd64" ] ; then
+    mount $AMD_BOOT /mnt
+    if [ -f "$AMD_KMS_CONF_FILE" ] ; then
         echo "/dev/sda1 mounted succesful!"
 
-        if [ grep -q "initrd.img" $AMD_KMS_CONF_FILE ]; then
+        if grep -q "initrd.img" "$AMD_KMS_CONF_FILE" ; then
             echo "KMS file verified succesful!"
             #TODO change with mv
-            cp $KMS_CONF $HOME_OMBORI/.old_gridos.conf-$DATE
+            cp "$KMS_CONF" "$HOME_OMBORI/.old_gridos.conf-$DATE"
 
 
         else
@@ -33,21 +32,21 @@ if [ "$SYSTEM_ARCH" == "amd64" ] then
             exit 1
         fi
     else
-        echo "ERR! /dev/sda1 CAN'T mount!"
+        echo "ERR! /dev/sda1 CANT mount!"
         echo "Contact with Ombori Tech team!"
         exit 1
     fi
-#ARM64 arch systems
-elif [ "$SYSTEM_ARCH" == "arm64" ] then
 
-    mount /dev/mmcblk0p1 /mnt
-    if [ -f "$ARM_KMS_CONF_FILE" ]; then
+#ARM64 arch systems
+elif [ "$SYSTEM_ARCH" = "arm64" ] ; then
+    mount $ARM_BOOT /mnt
+    if [ -f "$ARM_KMS_CONF_FILE" ] ; then
         echo "/dev/mmcblk0p1 mounted succesful!"
 
-        if [ grep -q "initrd.img" $ARM_KMS_CONF_FILE ]; then
+        if grep -q "initrd.img" "$ARM_KMS_CONF_FILE" ; then
             echo "KMS file verified succesful!"
             #TODO change with mv
-            cp $ARM_KMS_CONF_FILE $HOME_OMBORI/.old_config.txt-$DATE
+            cp "$ARM_KMS_CONF_FILE" "$HOME_OMBORI/.old_config.txt-$DATE"
 
 
         else
