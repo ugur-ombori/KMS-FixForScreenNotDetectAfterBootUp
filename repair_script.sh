6d696fec3b0c08a7ef4a670a64ef0f52bcb180ed
ugur-ombori <97463402+ugur-ombori@users.noreply.github.com>
2022-04-01 14:46:32 +0300

    arm support init

--- repair_script.sh
+++ repair_script.sh
@@ -7,14 +7,15 @@ ARM_KMS_CONF_FILE=/mnt/config.txt
 DATE=`TZ='Europe/Stockholm' date +%Y-%b-%d.%H:%M:%S`
 HOME_OMBORI=`getent passwd ombori | cut -d: -f6`
 
-#Boot devices for arm and amd
+#Boot devices for arm and amd (diffrence comes from using disk and sd card)
 AMD_BOOT=/dev/sda1
 ARM_BOOT=/dev/mmcblk0p1
 
 SYSTEM_ARCH=`dpkg --print-architecture`
 echo "System Architecture: $SYSTEM_ARCH"
 
-if [[ $SYSTEM_ARCH == "amd64" ]]
+#AMD64 arch systems
+if [ $SYSTEM_ARCH == "amd64" ] then
 
     mount /dev/sda1 /mnt
     if [ -f "$AMD_KMS_CONF_FILE" ]; then
@@ -22,6 +23,7 @@ if [[ $SYSTEM_ARCH == "amd64" ]]
 
         if [ grep -q "initrd.img" $AMD_KMS_CONF_FILE ]; then
             echo "KMS file verified succesful!"
+            #TODO change with mv
             cp $KMS_CONF $HOME_OMBORI/.old_gridos.conf-$DATE
 
 
@@ -35,4 +37,29 @@ if [[ $SYSTEM_ARCH == "amd64" ]]
         echo "Contact with Ombori Tech team!"
         exit 1
     fi
+#ARM64 arch systems
+elif [ $SYSTEM_ARCH == "arm64" ] then
+
+    mount /dev/mmcblk0p1 /mnt
+    if [ -f "$ARM_KMS_CONF_FILE" ]; then
+        echo "/dev/mmcblk0p1 mounted succesful!"
+
+        if [ grep -q "initrd.img" $ARM_KMS_CONF_FILE ]; then
+            echo "KMS file verified succesful!"
+            #TODO change with mv
+            cp $ARM_KMS_CONF_FILE $HOME_OMBORI/.old_config.txt-$DATE
+
+
+        else
+            echo "ERR! KMS file could NOT verified!!"
+            echo "Contact with Ombori Tech team!"
+            exit 1
+        fi
+    else
+        echo "ERR! /dev/mmcblk0p1 CAN'T mount!"
+        echo "Contact with Ombori Tech team!"
+        exit 1
+    fi
+
+else
 fi
